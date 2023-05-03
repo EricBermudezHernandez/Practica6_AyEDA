411b2a6310fca774a31663774858522d3adf7093
Eric Bermúdez <91663286+EricBermudezHernandez@users.noreply.github.com>
2023-05-04 00:55:40 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,65 +1,48 @@
+#ifndef NODOS_H
+#define NODOS_H
+
 #include <iostream>
 
-#include "arboles.hpp"
-#include "nodos.hpp"
+// Clase Genérica Nodo Binario
+template <class Key>
+class NodoB {
+ public:
+  // Constructor
+  NodoB(const Key& k, NodoB<Key>* izquierdo = NULL, NodoB<Key>* derecho = NULL)
+      : dato_(k), izquierdo_(izquierdo), derecho_(derecho) {}
+  // Getters
+  Key GetDato() const { return dato_; }
+  NodoB<Key>* GetIzquierdo() const { return izquierdo_; }
+  NodoB<Key>* &GetIzquierdo() { return izquierdo_; }
 
-int main() {
-  #ifdef TRAZA
-    std::cout << "MODO TRAZA" << std::endl;
-  #endif
-  int opcion;
-  long clave;
-  std::cout << "¿Qué tipo de árbol binario quiere crear? \n 1) ABB \n 2) AVL\n";
-  std::cin >> opcion;
-  AB<long>* arbol;
-  if (opcion == 1) {
-    arbol = new ABB<long>{};
-  } else if (opcion == 2) {
-    arbol = new AVL<long>{};
-  } else {
-    std::cout << "La opción " << opcion << " es incorrecta" << std::endl;
-    return 1;
-  }
+  NodoB<Key>* GetDerecho() const { return derecho_; }
+  NodoB<Key>* &GetDerecho() { return derecho_; }
 
-  do {
-    std::cout
-        << "Elija una opción:\n[0] Salir\n[1] Insertar clave\n[2] Buscar "
-           "clave\n[3] Mostrar árbol inorden\n";
-    std::cin >> opcion;
-    switch (opcion) {
-      case 0:
-        break;
+ protected:
+  Key dato_;
+  NodoB<Key>* izquierdo_;
+  NodoB<Key>* derecho_;
+};
 
-      case 1:  // Inserción
-        std::cout << "Introduzca el valor de la clave: ";
-        std::cin >> clave;
-        arbol->Insertar(clave);
-        std::cout << "Árbol vacío\nNivel 0: [.]\nInsertar: " << clave << std::endl;
-        std::cout << *arbol << std::endl;
-        break;
-
-      case 2:  // Búsqueda
-        std::cout << "Introduzca el valor de la clave que quiera buscar: ";
-        std::cin >> clave;
-        if (arbol->Buscar(clave)) {
-          std::cout << "Clave encontrada" << std::endl;
-        } else {
-          std::cout << "Clave no encontrada" << std::endl;
-        }
-        break;
-
-      case 3:  // Mostrar árbol inorden
-        std::cout << "Recorrido inorden del árbol:" << std::endl;
-        arbol->Inorden(arbol->GetRaiz());
-        std::cout << std::endl;
-        break;
-
-      default:
-        std::cout << "La opción introducida " << opcion << " es incorrecta"
-                  << std::endl;
-        break;
-    }
-  } while (opcion != 0);
-  delete arbol; // Liberamos la memoria del árbol una vez acabe el programa
-  return 0;
+template<class Key>
+std::ostream& operator<<(std::ostream& os, const NodoB<Key>& nodo) {
+  return os << nodo.GetDato();
 }
+
+// Clase Nodo AVL
+template <class Key>
+class NodoAVL : public NodoB<Key> {
+ public:
+  // Constructor:
+  NodoAVL(const Key& k, NodoB<Key>* izquierdo = NULL,
+          NodoB<Key>* derecho = NULL, int factor_balanceo = 0)
+      : NodoB<Key>(k, izquierdo, derecho), factor_balanceo_(factor_balanceo) {}
+
+  int GetBal() const { return factor_balanceo_; }
+  void SetBal(const int factor_balanceo) { factor_balanceo_ = factor_balanceo; }
+
+ private:
+  int factor_balanceo_;  // Factor de balanceo del nodo
+};
+
+#endif
